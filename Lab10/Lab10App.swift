e54636e9e6c82ce4a0dbff8385af566d0b52605c
Daniel Cubillo <dcubillo81@gmail.com>
2021-03-06 10:50:13 -0600

    Initial Commit

---
+++ Lab10/Lab10App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab10App.swift
+//  Lab10
+//
+//  Created by Daniel Cubillo on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Lab10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
